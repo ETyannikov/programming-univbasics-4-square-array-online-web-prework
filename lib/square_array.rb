787e54b6029b2fddd34e0eeaac6bd39ec10f8847
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-06 17:42:30 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0
  
   while counter < array.length do
-    print array[counter]^2
+    new_array[counter] = array[counter]^2
     counter += 1
   end
+  return new_array
 end
